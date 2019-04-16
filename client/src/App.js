08365049c9394180a495e21f185131c2753b8b65
shahrukhbari99 <201552066@iiitvadodara.ac.in>
2019-04-16 16:09:38 +0530

    app changed

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,25 @@ import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+
   // import Rental frpm './contracts/RentableObjects.json'
+
 
 const styles = {
   root: {
     flexGrow: 1,
   },
+
+  appbar: {
+    alignItems: 'center',
+    height:100,
+  },
+
+  title: {
+    padding:50,
+    fontSize: '2rem',
+  },
+
 };
 
 function SimpleAppBar(props) {
@@ -17,9 +30,9 @@ function SimpleAppBar(props) {
 
   return (
     <div className={classes.root}>
-      <AppBar position="static" color="default">
-        <Toolbar>
-          <Typography variant="h6" color="inherit">
+      <AppBar position="static" className={classes.appbar}>
+        <Toolbar variant="dense" color='#1e88e5' container>
+          <Typography variant="h5" color="inherit" className={classes.title}>
             Rental Services
           </Typography>
         </Toolbar>
